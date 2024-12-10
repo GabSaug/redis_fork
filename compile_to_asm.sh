923d70521311833e49b48fd860edd12f0f8e5616
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-12-10 14:17:31 +0100

    Updated compile file to fix include bugs

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -3,7 +3,7 @@
 # make clean > /dev/null
 make distclean > /dev/null
 opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
-if ! gcc -masm=intel -std=gnu11 -Ideps/hdr_histogram/ -Wall -Wextra -Wsign-compare -Wundef -Wno-format-zero-length -Wpointer-arith -Wno-missing-braces -Wno-missing-field-initializers -Wno-missing-attributes -pipe -g3 -fvisibility=hidden -Wimplicit-fallthrough -funroll-loops $opt -S -D_GNU_SOURCE -D_REENTRANT -Iinclude -Ideps/jemalloc/include/jemalloc -Iinclude -I/usr/include/lua5.1 -DJEMALLOC_NO_PRIVATE_NAMESPACE -o "$2" "$1"; then
+if ! gcc -masm=intel -std=gnu11 -Ideps/hdr_histogram/ -Wall -Wextra -Wsign-compare -Wundef -Wno-format-zero-length -Wpointer-arith -Wno-missing-braces -Wno-missing-field-initializers -Wno-missing-attributes -pipe -g3 -fvisibility=hidden -Wimplicit-fallthrough -funroll-loops $opt -S -D_GNU_SOURCE -D_REENTRANT -Iinclude -Ideps/jemalloc/include/jemalloc -Ideps/fpconv -Ideps/hiredis -Ideps/hdr_histogram -Ideps/linenoise -Isrc -Iinclude -I/usr/include/lua5.1 -DJEMALLOC_NO_PRIVATE_NAMESPACE -o "$2" "$1"; then
 	echo "error compile to asm"
 	exit 1
 fi
