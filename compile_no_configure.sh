8302ba7a9faefdaaf2039afc60fc80936bc91f41
SAUGER Gabriel <gabriel.sauger@loria.fr>
2025-04-09 15:59:04 +0200

    add no inlining option

--- compile_no_configure.sh
+++ compile_no_configure.sh
@@ -2,7 +2,7 @@ mkdir -p build/bin
 make clean > /dev/null
 make distclean > /dev/null
 rm build/bin/redis
-opt="$(echo $1 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -fno-inline -finline-limit=2"
+opt="$(echo $1 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -fno-inline -fno-inline -finline-limit=2"
 make EXTRA_CFLAGS=" $opt" -j -n &> log_make.txt
 if ! make EXTRA_CFLAGS="$opt" -j ; then
 	echo "error make"
